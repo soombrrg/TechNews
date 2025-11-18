9df1e6842d11ea4d7db2ec04f3c00288335fced5
Soombrrg <slav.volchanin@mail.ru>
2025-11-18 18:08:24 +0300

    chore: fmt

--- src/app/celery.py
+++ src/app/celery.py
@@ -6,9 +6,7 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 celery = Celery("app")
-
 celery.config_from_object("django.conf:settings", namespace="CELERY")
-
 celery.autodiscover_tasks()
 
 
--- src/payments/urls.py
+++ src/payments/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 
 from payments.api.views import (
     PaymentDetailView,
